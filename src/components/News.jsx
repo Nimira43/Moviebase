0424c0c37b432b65ac5ecdd1301a302e1ba8ed64
Nick Rabone <n.rabone72@aol.com>
2024-06-30 11:59:25 +0100

    Starting on landing page.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const News = () => {
   return (
-    <div>
-      <h1>News</h1>
+    <div className='news'>
+        <header className="news-header">Views & News</header>
+        <div className="news-content">
+            <div className="navbar">
+                <div className="user"></div>
+                <nav className="categories">Categories</nav>
+            </div>
+            <div className="news-section">
+                  <div className="headline">Headline</div>
+                  <div className="news-grid">News Grid</div>
+              </div>  
+              <div className="my-blogs">My Blogs</div>
+        </div>  
     </div>
   )
 }
